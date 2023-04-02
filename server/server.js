7cdc0a3968d79d2359eac31ee03d010b2e2241cf
DevAlgot <algot.gronvall@hotmail.com>
2023-04-03 00:33:58 +0200

    What

--- server/server.js
+++ server/server.js
@@ -56,5 +56,5 @@ while (true) {
       res.status(500).send(error || 'Something went wrong');
     }
   })
+  app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
 }
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
