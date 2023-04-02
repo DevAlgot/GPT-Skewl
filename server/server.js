eede8ef86adc54ed036f6bcf94fe973377f0fb17
DevAlgot <algot.gronvall@hotmail.com>
2023-04-03 00:31:35 +0200

    Developed context

--- server/server.js
+++ server/server.js
@@ -21,29 +21,40 @@ app.get('/', async (req, res) => {
   })
 })
 
-let conversation = [{ "role": "system", "content": `You are a wonderful AI` }]
+const history = [];
 
-app.post('/', async (req, res) => {
-  try {
-    const prompt = req.body.prompt;
+while (true) {
 
-    conversation.push = [{ "role": "user", "content": `${prompt}` }]
+  app.post('/', async (req, res) => {
+    try {
+      const prompt = req.body.prompt;
 
-    const response = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: conversation //[{ "role": "user", "content": `${prompt}` }],
-    });
+      const messages = [];
+      for (const [input_text, completion_text] of history) {
+        messages.push({ role: "user", content: input_text });
+        messages.push({ role: "assistant", content: completion_text });
+      }
+  
+      messages.push({ role: "user", content: `${prompt}` });
+  
+    
+      const response = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: messages, //[{ "role": "user", "content": `${prompt}` }],
+      });
 
-    conversation.push = [{ "role": "assistant", "content": response.data.choices[0].message.content }]
+      conversation.push = [{ "role": "assistant", "content": response.data.choices[0].message.content }]
 
-    res.status(200).send({
-      bot: response.data.choices[0].message.content
-    });
+      res.status(200).send({
+        bot: response.data.choices[0].message.content
+      });
 
-  } catch (error) {
-    console.error(error)
-    res.status(500).send(error || 'Something went wrong');
-  }
-})
+      history.push([prompt, response.data.choices[0].message.content])
 
+    } catch (error) {
+      console.error(error)
+      res.status(500).send(error || 'Something went wrong');
+    }
+  })
+}
 app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
