a11c7754c8f84475607074ce86e8bd0fc809e7ab
DevAlgot <algot.gronvall@hotmail.com>
2023-04-02 19:38:33 +0200

    Maybe added context

--- server/server.js
+++ server/server.js
@@ -21,19 +21,19 @@ app.get('/', async (req, res) => {
   })
 })
 
+conversation = [{"role": "system", "content": "Hello there!"}]
+
 app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
+    conversation.append({"role": "user", "content": `${prompt}`})
     const response = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      messages: [{"role": "user", "content": `${prompt}`}],
-      //temperature: 0, // Higher values means the model will take more risks.
-      //max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      //top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      //frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      //presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+      messages: conversation     //[{"role": "user", "content": `${prompt}`}]
     });
+
+    conversation.append({"role": "assistant", "content": response.data.choices[0].messsage.content})
 
     res.status(200).send({
       bot: response.data.choices[0].message.content
