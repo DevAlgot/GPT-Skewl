ae12a6d18cec5420891383174417b3ba2b66db69
DevAlgot <algot.gronvall@hotmail.com>
2023-04-03 01:00:41 +0200

    Testing

--- server/server.js
+++ server/server.js
@@ -5,11 +5,11 @@ import { Configuration, OpenAIApi } from 'openai'
 
 dotenv.config()
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+//const configuration = new Configuration({
+//  apiKey: process.env.OPENAI_API_KEY,
+//});
 
-const openai = new OpenAIApi(configuration);
+//const openai = new OpenAIApi(configuration);
 
 const app = express()
 app.use(cors({ origin: '*' }))
@@ -22,11 +22,55 @@ app.get('/', async (req, res) => {
 })
 
 const history = [];
-while (true){
 
-  app.post('/', async (req, res) => {
+//while (true){
+//
+//  app.post('/', async (req, res) => {
+//
+//    const prompt = req.body.prompt;
+//
+//    const messages = [];
+//    for (const [input_text, completion_text] of history) {
+//      messages.push({ role: "user", content: input_text });
+//      messages.push({ role: "assistant", content: completion_text });
+//    }
+//
+//    messages.push({ role: "user", content: `${prompt}` });
+//
+//    try {
+//
+//      const response = await openai.createChatCompletion({
+//        model: "gpt-3.5-turbo",
+//        messages: messages, //[{ "role": "user", "content": `${prompt}` }],
+//      });
+//
+//      messages.push = [{ "role": "assistant", "content": response.data.choices[0].message.content }]
+//
+//      res.status(200).send({
+//        bot: response.data.choices[0].message.content
+//      });
+//
+//      history.push([prompt, response.data.choices[0].message.content])
+//
+//    } catch (error) {
+//      console.error(error)
+//      res.status(500).send(error && 'Something went wrong');
+//    }
+//  })
+//  app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+//}
 
-    const prompt = req.body.prompt;
+
+(async () => {
+  const configuration = new Configuration({
+    apiKey: "process.env.OPEN_API_KEY",
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const history = [];
+
+  while (true) {
+    const user_input = readlineSync.question("Your input: ");
 
     const messages = [];
     for (const [input_text, completion_text] of history) {
@@ -34,27 +78,25 @@ while (true){
       messages.push({ role: "assistant", content: completion_text });
     }
 
-    messages.push({ role: "user", content: `${prompt}` });
+    messages.push({ role: "user", content: user_input });
 
     try {
-
-      const response = await openai.createChatCompletion({
+      const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
-        messages: messages, //[{ "role": "user", "content": `${prompt}` }],
+        messages: messages,
       });
 
-      messages.push = [{ "role": "assistant", "content": response.data.choices[0].message.content }]
+      const completion_text = completion.data.choices[0].message.content;
+      console.log(completion_text);
 
-      res.status(200).send({
-        bot: response.data.choices[0].message.content
-      });
-
-      history.push([prompt, response.data.choices[0].message.content])
-
+      history.push([user_input, completion_text]);
     } catch (error) {
-      console.error(error)
-      res.status(500).send(error && 'Something went wrong');
+      if (error.response) {
+        console.log(error.response.status);
+        console.log(error.response.data);
+      } else {
+        console.log(error.message);
+      }
     }
-  })
-  app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
-}
+  }
+})();
