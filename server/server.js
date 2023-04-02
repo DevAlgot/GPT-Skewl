da25a2cfb7ed4e615592b67156cbdfd7f4c79cd6
DevAlgot <algot.gronvall@hotmail.com>
2023-04-03 00:37:40 +0200

    Help

--- server/server.js
+++ server/server.js
@@ -23,38 +23,38 @@ app.get('/', async (req, res) => {
 
 const history = [];
 
-while (true) {
 
-  app.post('/', async (req, res) => {
-    try {
-      const prompt = req.body.prompt;
 
-      const messages = [];
-      for (const [input_text, completion_text] of history) {
-        messages.push({ role: "user", content: input_text });
-        messages.push({ role: "assistant", content: completion_text });
-      }
-  
-      messages.push({ role: "user", content: `${prompt}` });
-  
-    
-      const response = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: messages, //[{ "role": "user", "content": `${prompt}` }],
-      });
+app.post('/', async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
 
-      conversation.push = [{ "role": "assistant", "content": response.data.choices[0].message.content }]
-
-      res.status(200).send({
-        bot: response.data.choices[0].message.content
-      });
-
-      history.push([prompt, response.data.choices[0].message.content])
-
-    } catch (error) {
-      console.error(error)
-      res.status(500).send(error || 'Something went wrong');
+    const messages = [];
+    for (const [input_text, completion_text] of history) {
+      messages.push({ role: "user", content: input_text });
+      messages.push({ role: "assistant", content: completion_text });
     }
-  })
-  app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
-}
+
+    messages.push({ role: "user", content: `${prompt}` });
+
+
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: messages, //[{ "role": "user", "content": `${prompt}` }],
+    });
+
+    conversation.push = [{ "role": "assistant", "content": response.data.choices[0].message.content }]
+
+    res.status(200).send({
+      bot: response.data.choices[0].message.content
+    });
+
+    history.push([prompt, response.data.choices[0].message.content])
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(error || 'Something went wrong');
+  }
+})
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+
