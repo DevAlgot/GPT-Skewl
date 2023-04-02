578c0698712de8663063d1ec9d30235c2234bf49
DevAlgot <algot.gronvall@hotmail.com>
2023-04-02 23:25:30 +0200

    Maybe?

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const app = express()
-app.use(cors({origin: '*'}))
+app.use(cors({ origin: '*' }))
 app.use(express.json())
 
 app.get('/', async (req, res) => {
@@ -21,19 +21,20 @@ app.get('/', async (req, res) => {
   })
 })
 
+let conversation = [{ "role": "system", "content": `You are a wonderful AI` }]
+
 app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
+    conversation.push = [{ "role": "user", "content": `${prompt}` }]
+
     const response = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      messages: [{"role": "user", "content": `${prompt}`}],
-      //temperature: 0, // Higher values means the model will take more risks.
-      //max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      //top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      //frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      //presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+      messages: [{ "role": "user", "content": `${prompt}` }],
     });
+
+    conversation.push = [{ "role": "assistant", "content": response.data.choices[0].message.content }]
 
     res.status(200).send({
       bot: response.data.choices[0].message.content
