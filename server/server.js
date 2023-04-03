4a519c3c795d4f31353a87613f3e2fa1504e8dcd
DevAlgot <algot.gronvall@hotmail.com>
2023-04-03 19:34:36 +0200

    js

--- server/server.js
+++ server/server.js
@@ -5,11 +5,10 @@ import { Configuration, OpenAIApi } from 'openai'
 
 dotenv.config()
 
-//const configuration = new Configuration({
-//  apiKey: process.env.OPENAI_API_KEY,
-//});
-
-//const openai = new OpenAIApi(configuration);
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+const openai = new OpenAIApi(configuration);
 
 const app = express()
 app.use(cors({ origin: '*' }))
@@ -20,6 +19,7 @@ app.get('/', async (req, res) => {
     message: 'HELLO!'
   })
 })
+
 
 
 
@@ -46,51 +46,4 @@ app.post('/', async (req, res) => {
 })
 app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
 
-//app.post('/', async (req, res) => {
-//
-//  const configuration = new Configuration({
-//    apiKey: process.env.OPEN_API_KEY,
-//  });
-//  const openai = new OpenAIApi(configuration);
-//
-//  const history = [];
-//
-//  while (true) {
-//    const user_input = req.body.prompt;
-//
-//    const messages = [];
-//    for (const [input_text, completion_text] of history) {
-//      messages.push({ role: "user", content: `${input_text}` });
-//      messages.push({ role: "assistant", content: `${completion_text}` });
-//    }
-//
-//    messages.push({ role: "user", content: `${user_input}` });
-//
-//    try {
-//      const completion = await openai.createChatCompletion({
-//        model: "gpt-3.5-turbo",
-//        messages: messages,
-//      });
-//
-//      const completion_text = completion.choices[0].message.content;
-//
-//      res.status(200).send({
-//        bot: completion_text
-//      });
-//
-//      history.push([user_input, completion_text]);
-//
-//
-//    } catch (error) {
-//      if (error.response) {
-//        console.log(error.response.status);
-//        console.log(error.response.data);
-//      } else {
-//        console.log(error.message);
-//      }
-//    }
-//    if (error){
-//      return
-//    }
-//  }
-//});
+
